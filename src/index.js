9229d2b56994c5f99126e69ed00b651b39706a3c
mostafahmed <mostafa.hamed@tagaddod.com>
2023-04-18 00:54:18 +0200

    modify string

--- src/index.js
+++ src/index.js
@@ -67,7 +67,7 @@ if (app.get("env") === "development") {
 }
 
 app.get("/",async (req, res) => {
-  await redisClient.set("key", "hello it is from redis 2!");
+  await redisClient.set("key", "hello it is from node using docker hub!");
   // const value = await redisClient.get("key");
   // await redisClient.disconnect();
   res.send("mostafa1");
